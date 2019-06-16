b09a4caac5bf1804d9183fa83450e5306f21a698
Masahiro Kobayashi <kobayashi_mshr@koba-masa.com>
2019-06-16 22:30:43 +0900

    Windows用魚拓機能追加

---
+++ gyotaku.bat
@@ -0,0 +1,19 @@
+@echo off
+setlocal enabledelayedexpansion
+
+call :main
+
+
+exit /b
+
+:main
+
+    set /p inputDir=Input target folder path.:
+    set /p outputDir=Input output folder path.:
+
+    set timestamp=%date:/=%_%time:~0,8%
+    set timestamp=%timestamp::=%
+    set outputFileName=%outputDir%\gyotaku-%timestamp%.txt
+
+    echo Finish. Result file is '%outputFileName%'.
+    exit /b
